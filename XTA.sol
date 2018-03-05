114c1432c1d559143dc804136bf502ef60856e23
sarinc <sarinc@live.no>
2018-03-05 18:47:40 +0100

    .

--- XTA.sol
+++ XTA.sol
@@ -1,39 +1,36 @@
-pragma solidity ^0.4.16;
-
+pragma solidity 0.4.16;
 
 contract Token {
-
-
     /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
+    function totalSupply() public constant returns (uint256 supply) {}
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function balanceOf(address _owner) public constant returns (uint256 balance) {}
 
     /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function transfer(address _to, uint256 _value) public returns (bool success) {}
 
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
 
     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of wei to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function approve(address _spender, uint256 _value) public returns (bool success) {}
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {}
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
@@ -53,7 +50,7 @@ contract StandardToken is Token {
             balances[_to] += _value;
             Transfer(msg.sender, _to, _value);
             return true;
-        } else { return false; }
+        } else {return false;}
     }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
@@ -67,7 +64,7 @@ contract StandardToken is Token {
             allowed[_from][msg.sender] -= _value;
             Transfer(_from, _to, _value);
             return true;
-        } else { return false; }
+        } else {return false;}
     }
 
     function balanceOf(address _owner) constant returns (uint256 balance) {
@@ -117,7 +114,7 @@ contract XecureTransAction is StandardToken { // CHANGE THIS. Update the contrac
 
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function XecureTransAction() {
+    function XecureTransAction() public {
         balances[msg.sender] = 100000000000000000000000000;          
         // 100 000 000 Tokens (XTA) Give the creator all initial tokens. This is set to 1000 for example. 
         // If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
@@ -135,7 +132,7 @@ contract XecureTransAction is StandardToken { // CHANGE THIS. Update the contrac
         // The owner of the contract gets ETH
     }
 
-    function() payable {
+    function() public payable {
         totalEthInWei = totalEthInWei + msg.value;
         uint256 amount = msg.value * unitsOneEthCanBuy;
         if (balances[fundsWallet] < amount) {
@@ -152,7 +149,7 @@ contract XecureTransAction is StandardToken { // CHANGE THIS. Update the contrac
     }
 
     /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
 
@@ -164,7 +161,7 @@ contract XecureTransAction is StandardToken { // CHANGE THIS. Update the contrac
         //if (!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), 
         //msg.sender, _value, this, _extraData)) { throw; }
         if (!_spender.call(bytes4(bytes32(keccak256("receiveApproval(address,uint256,address,bytes)"))), 
-            msg.sender, _value, this, _extraData)) {revert;}
+            msg.sender, _value, this, _extraData)) {revert();}
 
         return true;
     }
